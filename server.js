588d41c3ae79bcc1997394e394d8150807634cd0
Kevin Kunkel <Christopher.Kunkel@traviscountytx.gov>
2021-05-21 14:56:13 -0500

    added animal posting

--- server.js
+++ server.js
@@ -1,69 +1,103 @@
-const express = require("express");
-const { animals } = require("./data/animals.json");
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const {animals} = require('./data/animals.json');
+
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 const filterByQuery = (query, animalsArray) => {
-  let personalityTraitsArray = [];
+    let personalityTraitsArray = [];
 
   let filteredResults = animalsArray;
 
-  if (query.personalityTraits) {
-    if (typeof query.personalityTraits === "string") {
-      personalityTraitsArray = [query.personalityTraits];
-    } else {
-      personalityTraitsArray = query.personalityTraits;
-    }
+  if (query.personalityTraits){
+      if (typeof query.personalityTraits === 'string'){
+          personalityTraitsArray = [query.personalityTraits];
+      }  else {
+          personalityTraitsArray = query.personalityTraits;
+      }
 
-    personalityTraitsArray.forEach((trait) => {
-      filteredResults = filteredResults.filter(
-        (animal) => animal.personalityTraits.indexOf(trait) !== -1
-      );
-    });
+      personalityTraitsArray.forEach(trait => {
+          filteredResults = filteredResults.filter(animal => animal.personalityTraits.indexOf(trait) !== -1);
+      });
   }
-  if (query.diet) {
-    filteredResults = filteredResults.filter(
-      (animal) => animal.diet === query.diet
-    );
+  if(query.diet){
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
   }
-  if (query.species) {
-    filteredResults = filteredResults.filter(
-      (animal) => animal.species === query.species
-    );
-  }
-  if (query.name) {
-    filteredResults = filteredResults.filter(
-      (animal) => animal.name === query.name
-    );
-  }
-  return filteredResults;
+  if(query.species){
+    filteredResults = filteredResults.filter(animal => animal.species === query.species);
+}
+if(query.name){
+    filteredResults = filteredResults.filter(animal => animal.name === query.name);
+}
+return filteredResults;
 };
 
 const findById = (id, animals) => {
-  const result = animals.filter((animal) => animal.id === id)[0];
-  return result;
+    const result = animals.filter((animal) => animal.id === id)[0];
+    return result;
+  };
+
+const createNewAnimal = (body, animalsArray) =>{
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(path.join(__dirname, './data/animals.json'), JSON.stringify({animals:animalsArray},null,2));
+    return animal;
 };
 
-app.get("/api/animals", (req, res) => {
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  console.log(req.query);
-  res.json(results);
+const validateAnimal = (animal) => {
+    if(!animal.name || typeof animal.name!=='string'){
+        return false;
+    }
+    if(!animal.diet || typeof animal.diet!=='string'){
+        return false;
+    }
+    if(!animal.species || typeof animal.species!=='string'){
+        return false;
+    }
+    if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+        return false;
+    }
+    return true;
+}
+
+app.get('/api/animals', (req,res) => {
+    let results = animals;
+    if (req.query){
+      results = filterByQuery(req.query,results);
+    }
+    console.log(req.query);
+    res.json(results);
 });
 
 app.get("/api/animals/:id", (req, res) => {
-  const result = findById(req.params.id, animals);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
 });
 
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    req.body.id = animals.length.toString();
+    if (!validateAnimal(req.body)){
+        res.status(400).send('The animal is no properly formatted.');
+    } else {
+    const animal = createNewAnimal(req.body,animals);
+    res.json(animal);
+    }
+  });
+
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+    console.log(`API server now on port ${PORT}!`);
 });
